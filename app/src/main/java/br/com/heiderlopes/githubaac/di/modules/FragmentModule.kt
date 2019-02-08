991fccb359780279123b9091afb059136bdf8db3
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-02-07 23:12:29 -0200

    Integracao

---
+++ app/src/main/java/br/com/heiderlopes/githubaac/di/modules/FragmentModule.kt
@@ -0,0 +1,13 @@
+package br.com.heiderlopes.githubaac.di.modules
+
+import br.com.heiderlopes.githubaac.ui.userprofile.UserProfileFragment
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class FragmentModule {
+
+    @ContributesAndroidInjector
+    abstract fun contributeUserProfileFragment(): UserProfileFragment
+
+}
